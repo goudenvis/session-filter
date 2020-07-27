1a7bea7843e3ab0592d46e1477b02d5608bc080f
vagrant <vagrant@homestead>
2020-07-27 10:13:29 +0000

    Add daterange filters Change has and get options

--- src/Filter.php
+++ src/Filter.php
@@ -2,6 +2,9 @@
 
 namespace Goudenvis\SessionFilter;
 
+use Carbon\Carbon;
+use Illuminate\Support\Arr;
+
 class Filter
 {
     protected static $search = 'search';
@@ -15,10 +18,7 @@ class Filter
      */
     public static function hasFilter($key): bool
     {
-        return (
-            !empty(session(self::$search)) &&
-            array_key_exists($key, session(self::$search))
-        );
+        return Arr::has(session(self::$search), $key);
     }
 
     /**
@@ -32,7 +32,7 @@ class Filter
     {
         if ( self::hasFilter($key) )
         {
-            return session(self::$search)[$key];
+            return Arr::get(session(self::$search), $key, []);
         }
         return [];
     }
@@ -46,5 +46,73 @@ class Filter
     public static function setFilter($key, $data): void
     {
         session(self::$search . $key, $data);
+    }
+
+
+    /**
+     * @param bool $fullWeeks
+     * @return array
+     */
+    public static function applyFilterDaterange($fullWeeks = false)
+    {
+        if (self::hasFilter('daterange')) {
+            $startDate = self::getFilter('daterange.start');
+            $startDate = session('search.daterange.start');
+            $endDate = session('search.daterange.end');
+        } else {
+            $startDate = now()->subMonth();
+            $endDate = now()->endOfDay();
+        }
+
+        if ($fullWeeks){
+            $startDate->startOfWeek();
+            $endDate->endOfWeek();
+        }
+
+        return self::makeDaterangeFromDates($startDate, $endDate);
+    }
+
+    /**
+     * @param Carbon $startdate
+     * @param Carbon $enddate
+     * @return array
+     *
+     * Get a array with dates between the given dates,
+     * including the given dates itself.
+     */
+    public static function makeDaterangeFromDates(Carbon $startdate, Carbon $enddate): array
+    {
+        return self::makeDaterange($startdate->diffInDays($enddate), $enddate);
+    }
+
+    /**
+     * @param int $days
+     * @param null $startDate
+     * @return array
+     *
+     * Make a daterange from numbers.
+     * On default start today, and get back 7 days
+     */
+    public static function makeDaterange($days = 7, $startDate = null): array
+    {
+        $collect = [];
+        $date = $startDate ?? Carbon::today();
+
+        for ($i=self::dateRange($days); $i >= 0; $i--){
+            $useDate = $date->copy();
+            $collect[] = $useDate->subDays($i)->toDateString();
+        }
+        return $collect;
+    }
+
+    /**
+     * @param int $days
+     * @return int
+     *
+     * Give the amount of dates
+     */
+    protected static function dateRange($days = 30): int
+    {
+        return Carbon::today()->subDays($days)->diffInDays(Carbon::today());
     }
 }
